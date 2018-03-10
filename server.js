82b8655ca77f52bbea34a0e303f1ed2131a9e07f
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-03-10 12:39:34 +0100

    use angular PathLocationStrategy

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const path = require('path');
+
 //Install express server
 const express = require('express');
 const app = express();
 
-// redirect all requests to use https
+// Redirect all requests to use https
 app.use(() => {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] !== 'https') {
@@ -16,6 +18,11 @@ app.use(() => {
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// Delegate all get requests to index.html, so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
