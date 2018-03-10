fb2676337316afab724177c2db75307222bfe901
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-03-10 12:25:00 +0100

    force SSL

--- server.js
+++ server.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const app = express();
 
+// redirect all requests to use https
+app.use(() => {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+       ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+});
+
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
