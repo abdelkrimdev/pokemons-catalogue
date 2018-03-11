82a45f419c7a076c4685fc1197d95ded19ba923b
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-03-11 19:37:36 +0100

    adding gesture support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app.routing';
-
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
 
 
 @NgModule({
@@ -13,8 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     AppRoutingModule,
-    BrowserAnimationsModule,
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
