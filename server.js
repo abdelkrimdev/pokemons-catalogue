9f11280d92250363f36937466bf365f14be52808
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-03-11 11:47:47 +0100

    redirect http to https

--- server.js
+++ server.js
@@ -1,27 +1,21 @@
-const path = require('path');
-
-//Install express server
+// Install express server
 const express = require('express');
 const app = express();
 
-// Redirect all requests to use https
-/*app.use(() => {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-});*/
-
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(`${__dirname}/dist`));
+
+// Redirect http to https
+app.get('/*',function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] != 'https' && process.env.NODE_ENV === 'production')
+    res.redirect(`https://${req.hostname}${req.url}`);
+  else
+    next() /* Continue to other routes if we're not redirecting */
+});
 
 // Delegate all get requests to index.html, so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile(`${__dirname}/dist/index.html`);
 });
 
 // Start the app by listening on the default Heroku port
