4f8a2a1c52105fa70b0e4756860afa8c910631ee
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-03-17 20:06:11 +0100

    remove the flex layout module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+
 import { AppRoutingModule } from './app.routing';
 import { MaterialModule } from './material/material.module';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
@@ -18,7 +18,6 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    FlexLayoutModule,
     MaterialModule
   ],
   providers: [],
