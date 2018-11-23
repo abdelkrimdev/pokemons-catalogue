9717cf1489874a12a920ca9b353c9d404de1d943
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-11-23 19:58:35 +0100

    create pokemons module

---
+++ src/app/pokemons/pokemons.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+
+@NgModule({
+  declarations: [
+    PokemonDetailsComponent,
+    PokemonListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PokemonsModule { }
