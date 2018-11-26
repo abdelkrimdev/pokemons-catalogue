ace0381c0c06d4df9c9a335bda8c5e996d38619d
Abdelkrim Dib <abdelkrim-dev@outlook.com>
2018-11-26 21:57:57 +0100

    import pokemons routes

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -4,13 +4,16 @@ import { CommonModule } from '@angular/common';
 import { PokemonDetailsComponent } from './pokemon-details/pokemon-details.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
+import { PokemonsRoutingModule } from './pokemons.routing';
+
 @NgModule({
   declarations: [
     PokemonDetailsComponent,
     PokemonListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    PokemonsRoutingModule
   ]
 })
 export class PokemonsModule { }
